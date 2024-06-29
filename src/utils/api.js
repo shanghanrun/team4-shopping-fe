c46cf02f471551cbd076c27aee0830e46f2489ac
shanghanrun <idim7@naver.com>
2024-06-29 18:20:06 +0900

    .

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 // 상황따라 주소 다름
 const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
-const PROXY_URL = process.env.REACT_APP_PROXY_URL+'/api';
+const PROXY_URL = `${process.env.REACT_APP_PROXY_URL}/api`;
+// const PROXY_URL = process.env.REACT_APP_PROXY_URL+'/api';
 
 const api = axios.create({
   baseURL: LOCAL_BACKEND+'/api', //로컬용
