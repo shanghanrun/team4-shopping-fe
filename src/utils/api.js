4eb5422d79c1e3cef1ef399e5523057c9f7fbb44
shanghanrun <idim7@naver.com>
2024-07-01 14:06:27 +0900

    클라우드타입으로 다이렉트로 연결하므로 redirects없앰

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 // 상황따라 주소 다름
-// const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
+const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
 const PROXY_URL = process.env.REACT_APP_PROXY_URL+'/api';
+// const REACT_APP_CLOUDTYPE_BACKEND_URL='https://port-0-team4-shopping-be-ly2ihcepd6d25f57.sel5.cloudtype.app'
 console.log('proxy_url', PROXY_URL)
 
 const api = axios.create({
   // baseURL: LOCAL_BACKEND+'/api', //로컬용
-  baseURL: PROXY_URL,
+  // baseURL: PROXY_URL,
+  baseURL: 'https://port-0-team4-shopping-be-ly2ihcepd6d25f57.sel5.cloudtype.app/api',
   headers: {
     authorization: `Bearer ${sessionStorage.getItem("token")}`,
   },
