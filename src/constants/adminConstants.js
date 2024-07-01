a73a6c1fad163f298a805fdf62d91fe2a6723bed
shanghanrun <idim7@naver.com>
2024-07-01 20:48:45 +0900

    .

--- src/constants/adminConstants.js
+++ src/constants/adminConstants.js
@@ -1,12 +1,12 @@
 export const newItemDays = 20; //신상품 판단 일수
-export const bestItemNo = 10; // saleCount가 가장 많은 것부터 10개를 뽑도록
+export const bestItemNo = 5; // saleCount가 가장 많은 것부터 10개를 뽑도록
 sessionStorage.setItem('newItemDays', 20)
-sessionStorage.setItem('bestItemNo', 10)
+sessionStorage.setItem('bestItemNo', 5)
 
 export const getNewItemDays = () => {
     return sessionStorage.getItem('newItemDays') ? parseInt(sessionStorage.getItem('newItemDays'), 10) : 20;
 };
 
 export const getBestItemNo = () => {
-    return sessionStorage.getItem('bestItemNo') ? parseInt(sessionStorage.getItem('bestItemNo'), 10) : 10;
+    return sessionStorage.getItem('bestItemNo') ? parseInt(sessionStorage.getItem('bestItemNo'), 5) : 10;
 };
